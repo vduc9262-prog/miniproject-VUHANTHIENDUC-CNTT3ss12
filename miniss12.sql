f70393fa217baa68601e9f285f4a19221ea119c4
Vuduc2907 <vduc9262@gmail.com>
2026-05-08 10:26:51 +0700

    Create index for latest posts retrieval
    Added an index to optimize queries for retrieving the latest posts by creation time.

--- miniss12.sql
+++ miniss12.sql
@@ -157,6 +157,10 @@ SELECT @new_post_id;
 CALL sp_GetFriends(1, 10, 0);
 
 
+-- Tạo Index cho truy vấn lấy bài đăng mới nhất theo thời gian
+CREATE INDEX idx_post_created_at 
+ON Posts(created_at DESC);
+
 
 
 
